4e66158115919b9ce89b4e6415cebb589ccb71cb
MohammedOmaraa <mohamed.ali.omaraa@gmail.com>
2026-07-31 03:44:01 +0300

    feat(application): add product DTOs
    - Add ProductDto for product data transfer
    - Add BrandDto for brand responses
    - Add TypeDto for product type responses
    - Define application-level data transfer models

---
+++ ECommerce.Application/DTO's/Products/BrandDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ECommerce.Application.DTO_s.Products
+{
+    public class BrandDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ ECommerce.Application/DTO's/Products/TypeDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ECommerce.Application.DTO_s.Products
+{
+    public class TypeDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
